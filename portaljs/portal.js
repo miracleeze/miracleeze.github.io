050202eadf6e3cd8d9c7a791b6b2bf0128b6cb8a
WDD 230 Test Student Account <miracleeze221@gmail.om>
2021-04-23 17:00:22 -0600

    updated homepage

---
+++ portaljs/portal.js
@@ -0,0 +1,6 @@
+let date = new Date ();
+let currentYear = date.getFullYear();
+document.querySelector(`#currentYear`).textContent = currentYear;
+
+let modifiedTime = document.lastModified;
+document.querySelector(`#modifiedTime`).textContent = modifiedTime;
