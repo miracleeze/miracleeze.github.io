90ef12d8efb5e3cf85ab93d3c7ba693ffe786df6
WDD 230 Test Student Account <miracleeze221@gmail.om>
2021-05-20 20:56:02 -0600

    update all

---
+++ lesson5/js/bom.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('#input').textContent = input;
+const button = document.querySelector('#Add Chapter').textContent = 'Add Chapter';
+const unordered list = document.querySelector('#listcontainer').textContent = listcontainer;
+'Add Chapter'.addEventListener('click', ('listen') => {
+    let listen = document.createElement('listen').textContent = listen;
+
+  // all of our code will go here
+});
