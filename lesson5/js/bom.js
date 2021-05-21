81b77ce04478d1397295282cd556b5a9cab2fa06
WDD 230 Test Student Account <miracleeze221@gmail.om>
2021-05-20 22:09:44 -0600

    update all

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,21 +1,17 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const list = document.querySelector("ul.list");
-
 button.addEventListener("click", () => {
   let item = input.value;
   input.value = "";
-
   const listItem = document.createElement("li");
-  const listText = document.createElement("span");
+  const sometext = document.createElement("span");
   const deletebutton = document.createElement("button");
-
-  listItem.appendChild(listText);
-  listText.textContent = item;
+  listItem.appendChild(sometext);
+  sometext.textContent = item;
   listItem.appendChild(deletebutton);
   deletebutton.textContent = "❌";
   list.appendChild(listItem);
-
   deletebutton.onclick = function (e) {
     list.removeChild(listItem);
   };
