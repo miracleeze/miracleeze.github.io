c9472eb26068756ef7fe64fa7e0d5fca67ce64a5
WDD 230 Test Student Account <miracleeze221@gmail.om>
2021-05-22 18:51:58 -0600

    update all

---
+++ lesson5/prestonjs/banner.js
@@ -0,0 +1,6 @@
+let thedate = new Date();
+if (thedate.getDay() == 5) {
+  document.querySelector("#banner").style.display = "block";
+} else {
+  document.querySelector("#banner").style.display = "none";
+}
