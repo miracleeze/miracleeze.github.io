8957fc2ae53e3b146626695bdce9f441aabe3152
WDD 230 Test Student Account <miracleeze221@gmail.om>
2021-06-14 21:12:39 -0600

    update all

--- lesson8/js/storm.js
+++ lesson8/js/storm.js
@@ -1,9 +1,3 @@
-function adjustingRating(rating) {
+function adjustRating(rating) {
   document.getElementById("ratingvalue").innerHTML = rating;
-}
-function selectResponse() {
-  let s = document.querySelector("#selected");
-  let sel = document.querySelector("#selectbrowser");
-  s.style.display = "block";
-  s.textContent = sel.value;
 }
