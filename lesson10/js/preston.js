95a61734023fb343c7a206634e55d3e14eaf2aa1
WDD 230 Test Student Account <miracleeze221@gmail.om>
2021-06-25 19:34:08 -0600

    update all

---
+++ lesson10/js/preston.js
@@ -0,0 +1,15 @@
+let date = new Date();
+let currentYear = date.getFullYear();
+document.querySelector(`#currentYear`).textContent = currentYear;
+
+let modifiedTime = document.lastModified;
+document.querySelector(`#modifiedTime`).textContent = modifiedTime;
+
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("hide");
+}
+//preston page - adds pancake reminder on fridays
+let banner = document.querySelector(".banner");
+document.addEventListener("DOMContentLoaded", () => {
+  if (date.getDay() == 5) banner.style.display = "block";
+});
