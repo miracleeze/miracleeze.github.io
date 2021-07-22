f95930b3f3a111ce08ad84d955474cf11519994f
WDD 230 Test Student Account <miracleeze221@gmail.om>
2021-07-22 16:23:33 -0600

    update all

--- Goldcoast/homepage/js/manu.js
+++ Goldcoast/homepage/js/manu.js
@@ -1,8 +1,20 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += "responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".topnav");
+  const topnav = document.querySelectorAll(".topnav li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+    //Animate links
+    topnav.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = "topnavFade 0.5s ease forward ${index / 7 + 2}s";
+      }
+    });
+  });
+};
+
+navSlide();
