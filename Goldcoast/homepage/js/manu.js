fc81643711fc6210a88ebbda9737239da6eff040
WDD 230 Test Student Account <miracleeze221@gmail.om>
2021-07-23 04:10:02 -0600

    update all

--- Goldcoast/homepage/js/manu.js
+++ Goldcoast/homepage/js/manu.js
@@ -11,8 +11,9 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation =
-          "navLinkFade 0.5s ease forwards ${index / 7 + 2}s";
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 2
+        }s`;
       }
     });
     //Burger Animation
